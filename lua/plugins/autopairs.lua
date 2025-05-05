2f749872a6a96e8b663cfe9d83bdf65a45259384
Krivoshapkin Eduard <buttersus@mail.ru>
2025-05-05 18:28:22 +0900

    feat: don't disable autopairs based on context

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "windwp/nvim-autopairs",
+    opts = {
+      -- I was annoyed with how randomly does nvim-autopairs work
+      -- I recommend anyone else to add same option
+      check_ts = false,
+    },
+  },
+}
