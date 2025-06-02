5d8a914356c379132d96c74ad9cadcebc19fa899
Krivoshapkin Eduard <buttersus@mail.ru>
2025-06-02 16:40:35 +0300

    perf: remove focus.nvim and disable Illuminate for certain filetypes

--- lua/polish.lua
+++ lua/polish.lua
@@ -3,3 +3,6 @@ vim.api.nvim_create_autocmd("BufEnter", {
   pattern = "*",
   command = [[set fo-=o]],
 })
+
+-- Optimization for *.txt and *.csv files
+vim.g.Illuminate_ftblacklist = { "txt", "csv" }
