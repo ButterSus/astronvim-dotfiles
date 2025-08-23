5fa362c3452794dfb5c3df92b8df22109e89084a
Krivoshapkin Eduard <buttersus@mail.ru>
2025-08-23 15:51:38 +0900

    feat: limit treesitter-context span

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,21 +2,30 @@
 
 ---@type LazySpec
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
-      "lua",
-      "vim",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "lua",
+        "vim",
+      },
+      highlight = {
+        enable = false,
+      },
+      indent = {
+        enable = true,
+        disable = { "python", "verilog", "systemverilog" },
+      },
+      textobjects = {
+        enable = false,
+      },
     },
-    highlight = {
-      enable = false,
-    },
-    indent = {
-      enable = true,
-      disable = { "python", "verilog", "systemverilog" },
-    },
-    textobjects = {
-      enable = false,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    opts = {
+      max_lines = 2,
     },
   },
 }
